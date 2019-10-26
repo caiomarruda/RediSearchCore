773549f448e48f7cc5882c90ae6750733c141e9f
Caio Arruda <caio.madeiradearruda@gmail.com>
2019-10-26 03:20:18 +0100

    Updated to .net core 3.0

--- RediSearchCore/Startup.cs
+++ RediSearchCore/Startup.cs
@@ -1,12 +1,12 @@
-﻿using RediSearchCore.Core.Interfaces;
-using RediSearchCore.Core.Services;
-using RediSearchCore.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Hosting;
+using RediSearchCore.Core.Interfaces;
+using RediSearchCore.Core.Services;
+using RediSearchCore.Infrastructure.Repositories;
 
 namespace RediSearchCore
 {
@@ -22,38 +22,45 @@ namespace RediSearchCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers();
             services.AddTransient<IAirportService, AirportService>();
             services.AddSingleton<IAirportsRepository>(c => new AirportsRepository(Configuration["redisConnection"]));
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "RediSearch .Net Core",
                     Description = "Some examples using RediSearch and .Net Core",
-                    TermsOfService = "None",
-                    Contact = new Contact() { Name = "Caio Arruda", Email = "caio.madeiradearruda@gmail.com", Url = "clabsolutions.com.br" }
+                    Title = "RediSearch .Net Core",
+                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    {
+                        Name = "Caio Arruda",
+                        Email = "caio.madeiradearruda@gmail.com"
+                    }
                 });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
