83024795e67adae25070ea8c0dd8087b417ffaf4
Caio Arruda <caio.madeiradearruda@gmail.com>
2022-06-24 03:03:08 -0300

    Revert "Update Program.cs"
    This reverts commit c4fa9c1679d652b654be0c8d144d5a647c3f8218.

--- RediSearchCore/Program.cs
+++ RediSearchCore/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace RediSearchCore
 {
@@ -7,14 +9,16 @@ namespace RediSearchCore
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).
+                UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT")).
+                Build();
+                
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
